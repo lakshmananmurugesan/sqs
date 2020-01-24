f5f8bb9e67c80c982206513989efc6e055abb4f6
Gayan <Admin@Gayans-MacBook-Pro.local>
2020-01-24 18:12:03 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# NodeJs SQS Send Message/Consume Example
+
+This is an example of how to use aws sqs with nodejs Using with `aws-sdk` and `sqs-consumer` 
+npm package.
+
+#### Prerequisite
+
+1. aws cli
+2. node with npm
+
+#### How to run
+
+1. export SQS_URL={your sqs url}
+2. npm install
+3. To send sample message to the sql run `node sqs-send-message.js`
+4. To listen sqs consumer run `node sqs-consumer.js`
+
+
+
---
+++ sqs-send-message.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+
+// Configure the region
+AWS.config.update({region: 'us-east-1'});
+
+// Create an SQS service object
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const queueUrl = process.env['SQS_URL'];
+
+let categoryData = {
+    name: 'Advertising',
+    id: '123',
+    Action: 'QUESTION_CATEGORY_UPDATE',
+    service: 'Question'
+}
+
+let sqsData = {
+    MessageAttributes: {
+      "categoryName": {
+        DataType: "String",
+        StringValue: categoryData.name
+      },
+      "categoryId": {
+        DataType: "String",
+        StringValue: categoryData.id
+      }
+    },
+    MessageBody: JSON.stringify(categoryData),
+    MessageGroupId: "CategoryUpdate",
+    MessageDeduplicationId: uuidv4(),
+    QueueUrl: queueUrl
+};
+
+// Send the order data to the SQS queue
+let sendSqsMessage = sqs.sendMessage(sqsData).promise();
+
+sendSqsMessage.then((data) => {
+    console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+}).catch((err) => {
+    console.log(`OrdersSvc | ERROR: ${err}`);
+});
